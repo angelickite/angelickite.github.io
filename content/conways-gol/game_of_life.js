e59f8ecbf4465667184c2f15bb7cb356ac2ae978
angelickite <angelickite@gmail.com>
2019-05-22 18:52:48 +0200

    GoL: adds teams

--- content/conways-gol/game_of_life.js
+++ content/conways-gol/game_of_life.js
@@ -10,15 +10,12 @@ var game_of_life = function (_, Kotlin) {
   var Unit = Kotlin.kotlin.Unit;
   var getCallableRef = Kotlin.getCallableRef;
   var numberToInt = Kotlin.numberToInt;
-  var Math_0 = Math;
+  var coerceIn = Kotlin.kotlin.ranges.coerceIn_e4yvb3$;
   var IntRange = Kotlin.kotlin.ranges.IntRange;
   var print = Kotlin.kotlin.io.print_s8jyv4$;
-  var Array_0 = Array;
-  var Random = Kotlin.kotlin.random.Random;
-  var random = Kotlin.kotlin.ranges.random_xmiyix$;
-  function clamp($receiver, min, max) {
-    return $receiver < min ? $receiver : $receiver > max ? max : $receiver;
-  }
+  var max = Kotlin.kotlin.collections.max_tmsbgo$;
+  var get_indices = Kotlin.kotlin.collections.get_indices_tmsbgo$;
+  var toList = Kotlin.kotlin.collections.toList_7wnvza$;
   function main() {
     (new JSApplication()).run();
   }
@@ -68,6 +65,7 @@ var game_of_life = function (_, Kotlin) {
     this.mouse = new V2(0.0, 0.0);
     this.mouseDown = false;
     this.placementMode = -1;
+    this.placementTeam = 1;
   }
   Object.defineProperty(JSApplication.prototype, 'canvas', {
     get: function () {
@@ -133,7 +131,7 @@ var game_of_life = function (_, Kotlin) {
     this.paused = true;
   };
   JSApplication.prototype.createGrid = function () {
-    var scale = 10;
+    var scale = 20;
     this.grid = new Grid(this.canvas.width / scale | 0, this.canvas.height / scale | 0);
     this.layout = new GridLayout(this.grid, this.canvas.offsetLeft, this.canvas.offsetTop, this.canvas.width, this.canvas.height);
   };
@@ -145,19 +143,15 @@ var game_of_life = function (_, Kotlin) {
   function JSApplication$render$lambda(this$JSApplication) {
     return function (x, y) {
       var alive = this$JSApplication.grid.current[x][y] > 0;
-      if (alive) {
-        var ageLimit = 8;
-        var age = clamp(this$JSApplication.grid.current[x][y], 0, ageLimit);
-        var t = ratio(age, ageLimit);
-        var r = 230 - 30 * t;
-        var g = 170 - 90 * t;
-        var b = 200 - 70 * t;
-        this$JSApplication.context.fillStyle = 'rgba(' + numberToInt(r) + ', ' + numberToInt(g) + ', ' + numberToInt(b) + ', 1)';
-        this$JSApplication.context.fillRect(x * this$JSApplication.layout.slotWidth, y * this$JSApplication.layout.slotHeight, this$JSApplication.layout.slotWidth, this$JSApplication.layout.slotHeight);
-      }
+      if (!alive)
+        return;
+      var color = this$JSApplication.getTeamColor_za3lpa$(this$JSApplication.grid.current[x][y]);
+      this$JSApplication.context.fillStyle = color;
+      this$JSApplication.context.fillRect(x * this$JSApplication.layout.slotWidth, y * this$JSApplication.layout.slotHeight, this$JSApplication.layout.slotWidth, this$JSApplication.layout.slotHeight);
       return Unit;
     };
   }
+  var Math_0 = Math;
   JSApplication.prototype.render = function () {
     var newWidth = window.innerWidth;
     var newHeight = window.innerHeight;
@@ -184,22 +178,36 @@ var game_of_life = function (_, Kotlin) {
       this.context.restore();
       this.context.save();
       this.context.font = '16pt Arial';
-      var msg = 'Controls: Mouse | [S]tart and [S]top | [R]eset';
+      var msg = 'Controls: Mouse | [1,2,3,4] Select Team | [S]tart and [S]top | [R]eset';
       var bounds = this.context.measureText(msg);
       var heightApproximation = this.context.measureText('A').width;
       this.context.fillStyle = 'rgba(255, 255, 255, 1)';
       this.context.fillText(msg, this.layout.width / 2.0 - bounds.width / 2.0, baseY + heightApproximation / 2.0);
       this.context.restore();
       var x = this.mouse.x / this.layout.slotWidth;
-      var slotX = clamp(numberToInt(Math_0.floor(x)), 0, this.grid.w);
+      var slotX = coerceIn(numberToInt(Math_0.floor(x)), 0, this.grid.w);
       var x_0 = this.mouse.y / this.layout.slotHeight;
-      var slotY = clamp(numberToInt(Math_0.floor(x_0)), 0, this.grid.h);
+      var slotY = coerceIn(numberToInt(Math_0.floor(x_0)), 0, this.grid.h);
       if (isWithinBounds(this.grid, slotX, slotY)) {
         this.context.save();
-        this.context.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        this.context.fillStyle = this.getTeamColor_za3lpa$(this.placementTeam);
         this.context.fillRect(slotX * this.layout.slotWidth, slotY * this.layout.slotHeight, this.layout.slotWidth, this.layout.slotHeight);
         this.context.restore();
       }
+    }
+  };
+  var IllegalStateException_init = Kotlin.kotlin.IllegalStateException_init_pdl1vj$;
+  JSApplication.prototype.getTeamColor_za3lpa$ = function (team) {
+    switch (team) {
+      case 1:
+        return 'rgba(220, 40, 40, 1)';
+      case 2:
+        return 'rgba(40, 220, 40, 1)';
+      case 3:
+        return 'rgba(40, 40, 220, 1)';
+      case 4:
+        return 'rgba(220, 220, 40, 1)';
+      default:throw IllegalStateException_init(team.toString());
     }
   };
   JSApplication.prototype.onKeyDown_9ojx7i$ = function (raw) {
@@ -212,10 +220,26 @@ var game_of_life = function (_, Kotlin) {
       case 'KeyR':
         this.reset();
         break;
+      case 'Digit1':
+      case 'Numpad1':
+        this.placementTeam = 1;
+        break;
+      case 'Digit2':
+      case 'Numpad2':
+        this.placementTeam = 2;
+        break;
+      case 'Digit3':
+      case 'Numpad3':
+        this.placementTeam = 3;
+        break;
+      case 'Digit4':
+      case 'Numpad4':
+        this.placementTeam = 4;
+        break;
     }
   };
   JSApplication.prototype.onMouseDown_9ojx7i$ = function (raw) {
-    var tmp$;
+    var tmp$, tmp$_0;
     var event = Kotlin.isType(tmp$ = raw, MouseEvent) ? tmp$ : throwCCE();
     var x = event.pageX - this.layout.x;
     var y = event.pageY - this.layout.y;
@@ -224,10 +248,16 @@ var game_of_life = function (_, Kotlin) {
     if (this.paused) {
       if (this.mouseDown) {
         var x_0 = x / this.layout.slotWidth;
-        var slotX = clamp(numberToInt(Math_0.floor(x_0)), 0, this.grid.w);
+        var slotX = coerceIn(numberToInt(Math_0.floor(x_0)), 0, this.grid.w);
         var x_1 = y / this.layout.slotHeight;
-        var slotY = clamp(numberToInt(Math_0.floor(x_1)), 0, this.grid.h);
-        this.grid.current[slotX][slotY] = this.grid.current[slotX][slotY] === 0 ? 1 : 0;
+        var slotY = coerceIn(numberToInt(Math_0.floor(x_1)), 0, this.grid.h);
+        if (this.grid.current[slotX][slotY] === 0)
+          tmp$_0 = this.placementTeam;
+        else if (this.grid.current[slotX][slotY] !== this.placementTeam)
+          tmp$_0 = this.placementTeam;
+        else
+          tmp$_0 = 0;
+        this.grid.current[slotX][slotY] = tmp$_0;
         this.placementMode = this.grid.current[slotX][slotY];
       }
     }
@@ -249,9 +279,9 @@ var game_of_life = function (_, Kotlin) {
     if (this.paused) {
       if (this.mouseDown) {
         var x_0 = x / this.layout.slotWidth;
-        var slotX = clamp(numberToInt(Math_0.floor(x_0)), 0, this.grid.w);
+        var slotX = coerceIn(numberToInt(Math_0.floor(x_0)), 0, this.grid.w);
         var x_1 = y / this.layout.slotHeight;
-        var slotY = clamp(numberToInt(Math_0.floor(x_1)), 0, this.grid.h);
+        var slotY = coerceIn(numberToInt(Math_0.floor(x_1)), 0, this.grid.h);
         this.grid.current[slotX][slotY] = this.placementMode;
       }
     }
@@ -281,6 +311,7 @@ var game_of_life = function (_, Kotlin) {
   function ratio(a, b) {
     return a / b;
   }
+  var Array_0 = Array;
   function Grid(w, h) {
     this.w = w;
     this.h = h;
@@ -326,6 +357,8 @@ var game_of_life = function (_, Kotlin) {
       }
     }
   }
+  var Random = Kotlin.kotlin.random.Random;
+  var random = Kotlin.kotlin.ranges.random_xmiyix$;
   function randomize$lambda(closure$rng, this$randomize) {
     return function (x, y) {
       this$randomize.current[x][y] = random(closure$rng, Random.Default);
@@ -336,27 +369,77 @@ var game_of_life = function (_, Kotlin) {
     var rng = new IntRange(0, 1);
     scan($receiver, randomize$lambda(rng, $receiver));
   }
+  function step$lambda$teamCount(closure$left, closure$right, closure$down, closure$up, closure$leftDown, closure$leftUp, closure$rightDown, closure$rightUp) {
+    return function (team) {
+      var count = 0;
+      if (closure$left === team)
+        count = count + 1 | 0;
+      if (closure$right === team)
+        count = count + 1 | 0;
+      if (closure$down === team)
+        count = count + 1 | 0;
+      if (closure$up === team)
+        count = count + 1 | 0;
+      if (closure$leftDown === team)
+        count = count + 1 | 0;
+      if (closure$leftUp === team)
+        count = count + 1 | 0;
+      if (closure$rightDown === team)
+        count = count + 1 | 0;
+      if (closure$rightUp === team)
+        count = count + 1 | 0;
+      return count;
+    };
+  }
+  var collectionSizeOrDefault = Kotlin.kotlin.collections.collectionSizeOrDefault_ba2ldo$;
+  var ArrayList_init = Kotlin.kotlin.collections.ArrayList_init_ww73n8$;
+  var random_0 = Kotlin.kotlin.collections.random_iscd7z$;
   function step$lambda(this$step) {
     return function (x, y) {
+      var xLeft = (x + this$step.w - 1 | 0) % this$step.w;
+      var xRight = (x + 1 | 0) % this$step.w;
+      var yDown = (y + this$step.h - 1 | 0) % this$step.h;
+      var yUp = (y + 1 | 0) % this$step.h;
+      var left = this$step.previous[xLeft][y];
+      var right = this$step.previous[xRight][y];
+      var down = this$step.previous[x][yDown];
+      var up = this$step.previous[x][yUp];
+      var leftDown = this$step.previous[xLeft][yDown];
+      var leftUp = this$step.previous[xLeft][yUp];
+      var rightDown = this$step.previous[xRight][yDown];
+      var rightUp = this$step.previous[xRight][yUp];
       var neighbours = 0;
-      var left = (x + this$step.w - 1 | 0) % this$step.w;
-      var right = (x + 1 | 0) % this$step.w;
-      var down = (y + this$step.h - 1 | 0) % this$step.h;
-      var up = (y + 1 | 0) % this$step.h;
-      neighbours = neighbours + (this$step.previous[left][y] > 0 ? 1 : 0) | 0;
-      neighbours = neighbours + (this$step.previous[right][y] > 0 ? 1 : 0) | 0;
-      neighbours = neighbours + (this$step.previous[x][down] > 0 ? 1 : 0) | 0;
-      neighbours = neighbours + (this$step.previous[x][up] > 0 ? 1 : 0) | 0;
-      neighbours = neighbours + (this$step.previous[left][down] > 0 ? 1 : 0) | 0;
-      neighbours = neighbours + (this$step.previous[right][down] > 0 ? 1 : 0) | 0;
-      neighbours = neighbours + (this$step.previous[left][up] > 0 ? 1 : 0) | 0;
-      neighbours = neighbours + (this$step.previous[right][up] > 0 ? 1 : 0) | 0;
-      var alive = this$step.previous[x][y] > 0;
-      if (alive && (2 <= neighbours && neighbours <= 3))
-        this$step.current[x][y] = this$step.previous[x][y] + 1 | 0;
-      else if (!alive && neighbours === 3)
-        this$step.current[x][y] = 1;
-      else
+      neighbours = neighbours + (left > 0 ? 1 : 0) | 0;
+      neighbours = neighbours + (right > 0 ? 1 : 0) | 0;
+      neighbours = neighbours + (down > 0 ? 1 : 0) | 0;
+      neighbours = neighbours + (up > 0 ? 1 : 0) | 0;
+      neighbours = neighbours + (leftDown > 0 ? 1 : 0) | 0;
+      neighbours = neighbours + (leftUp > 0 ? 1 : 0) | 0;
+      neighbours = neighbours + (rightDown > 0 ? 1 : 0) | 0;
+      neighbours = neighbours + (rightUp > 0 ? 1 : 0) | 0;
+      var teamCount = step$lambda$teamCount(left, right, down, up, leftDown, leftUp, rightDown, rightUp);
+      var team1 = teamCount(1);
+      var team2 = teamCount(2);
+      var team3 = teamCount(3);
+      var team4 = teamCount(4);
+      var strongestIndices = maxIndices(new Int32Array([team1, team2, team3, team4]));
+      var destination = ArrayList_init(collectionSizeOrDefault(strongestIndices, 10));
+      var tmp$;
+      tmp$ = strongestIndices.iterator();
+      while (tmp$.hasNext()) {
+        var item = tmp$.next();
+        destination.add_11rb$(1 + item | 0);
+      }
+      var strongestTeams = destination;
+      var previous = this$step.previous[x][y];
+      var alive = previous > 0;
+      if (alive && (2 <= neighbours && neighbours <= 3)) {
+        this$step.current[x][y] = strongestTeams.contains_11rb$(previous) ? previous : random_0(strongestTeams, Random.Default);
+      }
+       else if (!alive && neighbours === 3) {
+        this$step.current[x][y] = random_0(strongestTeams, Random.Default);
+      }
+       else
         this$step.current[x][y] = 0;
       return Unit;
     };
@@ -400,7 +483,50 @@ var game_of_life = function (_, Kotlin) {
     }
     print('\n');
   }
-  _.clamp_e4yvb3$ = clamp;
+  function max_0(values) {
+    return max(values);
+  }
+  function maxIndex(values) {
+    var tmp$;
+    var $receiver = get_indices(values);
+    var maxBy$result;
+    maxBy$break: do {
+      var iterator = $receiver.iterator();
+      if (!iterator.hasNext()) {
+        maxBy$result = null;
+        break maxBy$break;
+      }
+      var maxElem = iterator.next();
+      var maxValue = values[maxElem];
+      while (iterator.hasNext()) {
+        var e = iterator.next();
+        var v = values[e];
+        if (Kotlin.compareTo(maxValue, v) < 0) {
+          maxElem = e;
+          maxValue = v;
+        }
+      }
+      maxBy$result = maxElem;
+    }
+     while (false);
+    return (tmp$ = maxBy$result) != null ? tmp$ : null;
+  }
+  var ArrayList_init_0 = Kotlin.kotlin.collections.ArrayList_init_287e2$;
+  function maxIndices(values) {
+    var indices = ArrayList_init_0();
+    var max = -2147483648;
+    for (var index = 0; index !== values.length; ++index) {
+      var value = values[index];
+      if (value === max)
+        indices.add_11rb$(index);
+      else if (value > max) {
+        max = value;
+        indices.clear();
+        indices.add_11rb$(index);
+      }
+    }
+    return toList(indices);
+  }
   _.main = main;
   _.initalizeCanvas = initalizeCanvas;
   _.GridLayout = GridLayout;
@@ -415,6 +541,9 @@ var game_of_life = function (_, Kotlin) {
   package$gol.clear_nu4q73$ = clear;
   package$gol.isWithinBounds_3vi24z$ = isWithinBounds;
   package$gol.print_nu4q73$ = print_0;
+  _.max_pmhfmb$ = max_0;
+  _.maxIndex_pmhfmb$ = maxIndex;
+  _.maxIndices_pmhfmb$ = maxIndices;
   main();
   Kotlin.defineModule('game_of_life', _);
   return _;
